3f39979d390e4c55a7afe89e57ebe9118282db8d
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2024-11-17 17:23:26 +0530

    InitialFrontEnd

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/real state.svg" alt="" />
+          <span>PlotBlok</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        {user ? (
+          <div className="user">
+            <img
+              src="../../../public/pfp.png"
+              alt=""
+            />
+            <span>Ayush Singh Tomar</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Real Estate Investments Made Easy!</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>0+</h1>
+              <h2>Years of Ensurance</h2>
+            </div>
+            <div className="box">
+              <h1>1+</h1>
+              <h2>Users</h2>
+            </div>
+            <div className="box">
+              <h1>2+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
